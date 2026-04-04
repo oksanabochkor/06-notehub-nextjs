0259e7b272b70f95650ef2ff1a75b3313dade5d6
Oksana Bochkor <oksanabockor785@gmail.com>
2026-04-04 13:20:02 +0300

    fix build errors

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -4,7 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useParams } from "next/navigation";
 import { fetchNoteById } from "@/lib/api";
 
-import css from "./NoteDetails.module.css";
+import css from "../NoteDetails.module.css";
 
 export default function NoteDetailsClient() {
   const params = useParams();
